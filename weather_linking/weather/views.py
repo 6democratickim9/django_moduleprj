e9d9e658b5584af98b711fd0de7b37b26910f5b0
6democratickim9 <shcomon24@gmail.com>
2021-01-27 00:05:20 +0900

    modified

--- weather_linking/weather/views.py
+++ weather_linking/weather/views.py
@@ -4,29 +4,36 @@ from .models import City
 from .forms import CityForm
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=43fcd06479e874bea4eb2b675ab6b465'
-    city = 'Seoul'
+    url = 'https://api.openweathermap.org/data/2.5/onecall?lat=37.566535&lon=126.977969&exclude=minutely,hourly,alerts&appid=43fcd06479e874bea4eb2b675ab6b465'
+    
     
     if request.method == "POST":
         pass
 
-    form = CityForm()
+    r = requests.get(url).json()
+    current = r["current"]
+    daily_list = r["daily"]
 
-    cities = City.objects.all()
+    today_weather = {
+        "temp": round((current["temp"] - 271.94), 2),
+        "main": current["weather"][0]["main"],
+        "description" : current["weather"][0]["description"],
+        "icon": current["weather"][0]["icon"]
+    }
 
     weather_data = []
-
-    for city in cities:
-        r = requests.get(url.format(city)).json()
-
-        city_weather = {
-            'city': city.name,
-            'temperature': r["main"]["temp"],
-            'description': r["weather"][0]["description"],
-            'icon': r["weather"][0]["icon"],
+    for index, daily in enumerate(daily_list):
+        daily_weather = {
+            "day": index,
+            "main": daily["weather"][0]["main"],
+            "description" : daily["weather"][0]["description"],
+            "icon": daily["weather"][0]["icon"]
         }
-        weather_data.append(city_weather)
-
-    context = {'weather_data' : weather_data, 'form' : form}
+        weather_data.append(daily_weather)
+    del weather_data[0]
+    context = {
+        'weather_data' : weather_data, 
+        'today_weather' : today_weather    
+    }
 
     return render(request, 'weather/weather.html', context)
