26420d3271a61f7337a4a2c3030fcb7357fee39f
6democratickim9 <shcomon24@gmail.com>
2021-01-26 21:31:35 +0900

    modifying

--- multicampus_moduleprj/multicampus_moduleprj/items.py
+++ multicampus_moduleprj/multicampus_moduleprj/items.py
@@ -5,7 +5,12 @@
 
 import scrapy
 
-class MulticampusModuleprjItem(scrapy.Item):
-    title = scrapy.Field()
-    writer = scrapy.Field()
-    preview = scrapy.Field()
+class MulticampusModuleprjItem_it(scrapy.Item):
+    title_it = scrapy.Field()
+    writer_it = scrapy.Field()
+    preview_it = scrapy.Field()
+
+class MulticampusModuleprjItem_ec(scrapy.Item):
+    title_ec = scrapy.Field()
+    writer_ec = scrapy.Field()
+    preview_ec = scrapy.Field()
--- multicampus_moduleprj/multicampus_moduleprj/spiders/mybots.py
+++ multicampus_moduleprj/multicampus_moduleprj/spiders/mybots.py
@@ -1,5 +1,5 @@
+from multicampus_moduleprj.multicampus_moduleprj.items import MulticampusModuleprjItem_ec, MulticampusModuleprjItem_it
 import scrapy
-from multicampus_moduleprj.items import MulticampusModuleprjItem
 from scrapy.http import Request
 
 # def remove_space(titles:list) -> list:
@@ -16,24 +16,25 @@ from scrapy.http import Request
 
 URL_IT = 'https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=105&sid2=105/&page=%s'
 start_page = 1
-
-
+URL_EC='https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=101&sid2=101/&page=%s'
 
 class MybotsSpider(scrapy.Spider):
-    name = 'mybots'
+    name = 'ec_mybots'
     allowed_domains = ['naver.com']
-    start_urls = [URL_IT%start_page]
+    start_urls = [URL_EC%start_page]
     # print("------------------->", start_urls)
     def start_requests(self):
         # print("chk1----------------------------------")
-        for i in range(10):
-            yield Request(url=URL_IT%(i+start_page), callback=self.parse, dont_filter=True)
+        for i in range(5):
+            yield Request(url=URL_EC%(i+start_page), callback=self.parse, dont_filter=True)
 
     def parse (self,response):
         # test = response.xpath('').extract()
-        previews = response.css('.lede::text').extract()
-        writers = response.css('.writing::text').extract()
-        titles = response.xpath('//*[@id="main_content"]/div[2]/ul[2]/li/dl/dt[2]/a/text()').extract()
+        previews_ec = response.css('.lede::text').extract()
+        writers_ec = response.css('.writing::text').extract()
+        titles_upperec = response.xpath('//*[@id="main_content"]/div[2]/ul[2]/li/dl/dt[2]/a/text()').extract()
+        titles_npic_it = response.xpath('//*[@id="main_content"]/div[2]/ul[2]/li/dl/dt/a/text()').extract()
+
         # print("-------------->", test)
         # print("-------------->", writers)
         # print("-------------->", previews)
@@ -42,10 +43,47 @@ class MybotsSpider(scrapy.Spider):
         
        
         for idx in range(len(titles)):
-            item = MulticampusModuleprjItem()
+            item = MulticampusModuleprjItem_ec()
             item['preview'] = previews[idx]
             item['writer'] = writers[idx]
             item['title'] = titles[idx]
+            
+
+            items.append(item)
+        # print("--------------2>", items)
+        return items
+
+class MybotsSpider(scrapy.Spider):
+    name = 'it_mybots'
+    allowed_domains = ['naver.com']
+    start_urls = [URL_IT%start_page]
+    # print("------------------->", start_urls)
+    def start_requests(self):
+        # print("chk1----------------------------------")
+        for i in range(5):
+            yield Request(url=URL_IT%(i+start_page), callback=self.parse, dont_filter=True)
+
+    def parse (self,response):
+        # test = response.xpath('').extract()
+        previews_it = response.css('.lede::text').extract()
+        writers_it = response.css('.writing::text').extract()
+        titles_pic_it = response.xpath('//*[@id="main_content"]/div[2]/ul[2]/li/dl/dt[2]/a/text()').extract() 
+        titles_npic_it = response.xpath('//*[@id="main_content"]/div[2]/ul[2]/li/dl/dt/a/text()').extract()
+
+        # print("-------------->", test)
+        # print("-------------->", writers)
+        # print("-------------->", previews)
+        
+        items = []
+        
+       
+        for idx in range(len(titles_pic_it)):
+            item = MulticampusModuleprjItem_it()
+            item['preview'] = previews_it[idx]
+            item['writer'] = writers_it[idx]
+            item['title'] = titles_pic_it[idx]
+            item['title'] = titles_npic_it[idx]
+
             
 
             items.append(item)
